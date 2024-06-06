cd69dd5013e0c5ab0c4864d7daeef7c64dd57f55
Anderson <as533207@gmail.com>
2024-06-05 22:16:25 -0300

    variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,6 +1,6 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        double salarioMinimo = 250;
+        double salarioMinimo = 2500;
 
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
