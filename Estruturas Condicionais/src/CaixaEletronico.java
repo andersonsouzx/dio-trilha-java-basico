fbc2189d3da752f03b48f00a52496a4356a01845
Anderson <as533207@gmail.com>
2024-06-07 17:27:17 -0300

    if-else / switch-case

--- Estruturas Condicionais/src/CaixaEletronico.java
+++ Estruturas Condicionais/src/CaixaEletronico.java
@@ -4,7 +4,7 @@ public class CaixaEletronico {
     public static void main(String[] args) {
 
        double saldo = 25.0;
-       double valorSolicitado = 17.;
+       double valorSolicitado = 17.0;
 
        if(valorSolicitado < saldo)
         saldo = saldo - valorSolicitado;
