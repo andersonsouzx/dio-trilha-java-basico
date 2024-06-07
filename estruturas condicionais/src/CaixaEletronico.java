9f0869b955311db333217a5f2c782a912a38c342
Anderson <as533207@gmail.com>
2024-06-07 17:18:40 -0300

    #

--- estruturas condicionais/src/CaixaEletronico.java
+++ estruturas condicionais/src/CaixaEletronico.java
@@ -3,7 +3,7 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
 
-       double saldo = 25.0;
+       double saldo = 25.;
        double valorSolicitado = 17.0;
 
        if(valorSolicitado < saldo)
