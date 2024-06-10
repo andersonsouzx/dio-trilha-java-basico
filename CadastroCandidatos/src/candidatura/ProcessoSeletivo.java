983c4ca6d350234466642e4c2930672318773978
Anderson <as533207@gmail.com>
2024-06-10 13:52:30 -0300

    .

--- CadastroCandidatos/src/candidatura/ProcessoSeletivo.java
+++ CadastroCandidatos/src/candidatura/ProcessoSeletivo.java
@@ -11,7 +11,7 @@ public class ProcessoSeletivo {
 		if (salarioBase > salarioPretendido) {
 			System.out.println("LIGAR PARA O CANDIDATO");
 		}else if(salarioBase == salarioPretendido)
-			System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+			System.out.println("LIGA PARA O CANDIDATO COM CONTRA PROPOSTA");
 		else{
 			System.out.println("AGUARDADANDO O RESULTADO DOS DEMAIS CANDIDATOS");
 		}
